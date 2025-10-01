631be42d1debac88d549387bae6a6495b019d202
mathildesjo <mathildesjo@hotmail.com>
2025-10-01 14:01:05 +0200

    w

--- tema1/oppgave3.php
+++ tema1/oppgave3.php
@@ -8,5 +8,5 @@ $tall2=$_POST ["tall2"]; /* variable gitt verdier fra feltene i HTML-skjemaet */
 $sum=$tall1+$tall2;
 $differanse=$tall1-$tall2;
 
-print("Summen er $sum");
+print("Summen er $sum tihi");
 ?>
