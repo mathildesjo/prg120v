a792662e0e40c82f44fc4e81668dc43ecda25297
mathildesjo <mathildesjo@hotmail.com>
2025-10-13 13:15:07 +0200

    hhhh

--- tema2/oppgave1.php
+++ tema2/oppgave1.php
@@ -1,15 +1,23 @@
 <?php /* Oppgave 1 */
 /*
-/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Hva er 3 ganger 3 ?"
-/* Programmet sjekker om svaret er riktig og skriver ut en melding avhengig av om svaret er riktig eller feil
+/* Programmet mottar fra et HTML-skjema et svar på spørsmålet "Er du student (j/n) ?"
+/* Programmet sjekker om det er svart j eller n på spørsmålet og skriver ut en passende melding
 */
 $svar=$_POST ["svar"];
-if ($svar == 9) /* avgitt svar er riktig */
+if (!$svar) /* det er ikke svart på spørsmålet */
 {
-print("Riktig. 3 ganger 3 er 9 <br/>");
+print("Du har ikke svart p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 }
-else /* avgitt svar er feil */
+else if ($svar == "j") /* det er svart j på spørsmålet */
 {
-print("Feil. 3 ganger 3 er ikke $svar. 3 ganger 3 er 9 <br/>");
+print("Du har svart ja p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
+}
+else if ($svar == "n") /* det er svart n på spørsmålet */
+{
+print("Du har svart nei p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
+}
+else /* det er ikke svart verken j eller n på spørsmålet */
+{
+print("Du har ikke svart ja eller nei p&aring; sp&oslash;rsm&aring;let om du er student <br/>");
 }
 ?>
