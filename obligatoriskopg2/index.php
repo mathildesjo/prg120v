96de50b725a80b3139d3e0f0d0464d3209cb4de0
mathildesjo <mathildesjo@hotmail.com>
2025-10-22 11:26:22 +0200

    hh

--- obligatoriskopg2/index.php
+++ obligatoriskopg2/index.php
@@ -7,13 +7,14 @@
 <body>
     <h1>Velg funksjon</h1>
     <ul>
-        <li><a href="vis-klasse.php">Vis klasser</a></li>
-        <li><a href="vis-student.php">Vis studenter</a></li>
-        <li><a href="leggtil-klasse.php">Registrer ny klasse</a></li>
-        <li><a href="leggtil-student.php">Registrer ny student</a></li>
+        <li><a href="registrer-klasse.php">Registrer klasser</a></li>
+        <li><a href="registrer-student.php">Registrer student</a></li>
+        <li><a href="vis-alle-studenter.php">Vis alle studenter</a></li>
+        <li><a href="vis-alle-klasser.php">Vis alle klasser</a></li>
         <li><a href="slett-klasse.php">Slett klasse</a></li>
         <li><a href="slett-student.php">Slett student</a></li>
-        <li><a href="db-php"><Database-php></a></li>
+        <li><a href="vis-klasse.php">Vis klasse</a></li>
+        <li><a href="db-php">Database-php</a></li>
     </ul>
 </body>
 </html>
