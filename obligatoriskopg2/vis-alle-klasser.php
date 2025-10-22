f153fb3aacdd17c4d02eaec7139064a82128464e
mathildesjo <mathildesjo@hotmail.com>
2025-10-22 12:49:25 +0200

    hshsh

--- obligatoriskopg2/vis-alle-klasser.php
+++ obligatoriskopg2/vis-alle-klasser.php
@@ -1,3 +1,28 @@
-<?php 
-	print ("<h3>Vis alle klasser</h3>"); 
-?> 
+
+<?php  /* vis-alle-klasser */
+/*
+/*  Programmet skriver ut alle registrerte klasser
+*/
+  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+
+  $sqlSetning="SELECT * FROM klasse;";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+
+  print ("<h3>Registrerte klasser</h3>");
+  print ("<table border=1>");  
+  print ("<tr><th align=left>klasse</th> <th align=left>klasse</th></tr>"); 
+
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
+      $klasse=$rad["klasse"];        /* ELLER $klasse=$rad[0]; */
+      $klasse=$rad["klasse"];    /* ELLER $klasse=$rad[1]; */
+
+      print ("<tr> <td> $klasse </td> <td> $klasse </td> </tr>");
+    }
+  print ("</table>"); 
+?>
