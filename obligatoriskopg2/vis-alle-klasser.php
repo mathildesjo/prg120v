2be90b8359554f89d98698a5dc0550fce4fc2353
mathildesjo <mathildesjo@hotmail.com>
2025-10-22 19:39:41 +0200

    hhh

--- obligatoriskopg2/vis-alle-klasser.php
+++ obligatoriskopg2/vis-alle-klasser.php
@@ -1,28 +1,27 @@
-
 <?php  /* vis-alle-klasser */
 /*
 /*  Programmet skriver ut alle registrerte klasser
 */
-  include("db-tilkobling.php");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-  $sqlSetning="SELECT * FROM klasse;";
-  
-  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
-    /* SQL-setning sendt til database-serveren */
-	
-  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+$sqlSetning = "SELECT * FROM klasse;";
+$sqlResultat = mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; hente data fra databasen");
+/* SQL-setning sendt til database-serveren */
 
-  print ("<h3>Registrerte klasser</h3>");
-  print ("<table border=1>");  
-  print ("<tr><th align=left>klasse</th> <th align=left>klasse</th></tr>"); 
+$antallRader = mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
-  for ($r=1;$r<=$antallRader;$r++)
-    {
-      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
-      $klasse=$rad["klasse"];        /* ELLER $klasse=$rad[0]; */
-      $klasse=$rad["klasse"];    /* ELLER $klasse=$rad[1]; */
+print("<h3>Registrerte klasser</h3>");
+print("<table border=1>");  
+print("<tr><th align=left>Klassekode</th> <th align=left>Klassenavn</th> <th align=left>Studiumkode</th></tr>"); 
 
-      print ("<tr> <td> $klasse </td> <td> $klasse </td> </tr>");
-    }
-  print ("</table>"); 
+while ($rad = mysqli_fetch_array($sqlResultat))  /* henter hver rad fra resultatet */
+{
+    $klassekode = $rad["klassekode"];
+    $klassenavn = $rad["klassenavn"];
+    $studiumkode = $rad["studiumkode"];
+
+    print("<tr> <td>$klassekode</td> <td>$klassenavn</td> <td>$studiumkode</td> </tr>");
+}
+
+print("</table>"); 
 ?>
