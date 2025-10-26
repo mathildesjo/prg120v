6ee374eb9ad08ac9d7e0f4447cf3e59f1391596c
mathildesjo <mathildesjo@hotmail.com>
2025-10-26 15:41:05 +0100

    jjj

--- obligatoriskopg2/registrer-klasse.php
+++ obligatoriskopg2/registrer-klasse.php
@@ -1,51 +1,53 @@
-<?php  /* registrer-klasse */
+
+<?php  /* registrer-student */
 /*
-/*  Programmet lager et html-skjema for å registrere en klasse
-/*  Programmet registrerer data (klassekode, klassenavn og studiumkode) i databasen
+/*  Programmet lager et html-skjema for å registrere en student
+/*  Programmet registrerer data (brukernavn, fornavn, etternavn og klassekode) i databasen
 */
 ?> 
 
-<h3>Registrer klasse</h3>
+<h3>Registrer student</h3>
 
-<form method="post" action="" id="registrerKlasseSkjema" name="registrerKlasseSkjema">
+<form method="post" action="" id="registrerStudentSkjema" name="registrerStudentSkjema">
+  Brukernavn: <input type="text" id="brukernavn" name="brukernavn" required /> <br/>
+  Fornavn: <input type="text" id="fornavn" name="fornavn" required /> <br/>
+  Etternavn: <input type="text" id="etternavn" name="etternavn" required /> <br/>
   Klassekode: <input type="text" id="klassekode" name="klassekode" required /> <br/>
-  Klassenavn: <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
-  Studiumkode: <input type="text" id="studiumkode" name="studiumkode" required /> <br/>
-  <input type="submit" value="Registrer klasse" id="registrerKlasseKnapp" name="registrerKlasseKnapp" /> 
+  <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp" /> 
   <input type="reset" value="Nullstill" id="nullstill" name="nullstill" /> <br />
 </form>
 
 <?php 
-if (isset($_POST["registrerKlasseKnapp"]))
-{
-    $klassekode = $_POST["klassekode"];
-    $klassenavn = $_POST["klassenavn"];
-    $studiumkode = $_POST["studiumkode"];
+  if (isset($_POST["registrerStudentKnapp"]))
+  {
+      $brukernavn = $_POST["brukernavn"];
+      $fornavn = $_POST["fornavn"];
+      $etternavn = $_POST["etternavn"];
+      $klassekode = $_POST["klassekode"];
 
-    if (!$klassekode || !$klassenavn || !$studiumkode)
-    {
-        print("Alle felt m&aring; fylles ut (klassekode, klassenavn og studiumkode).");
-    }
-    else
-    {
-        include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+      if (!$brukernavn || !$fornavn || !$etternavn || !$klassekode)
+      {
+          print("Alle felt m&aring; fylles ut (brukernavn, fornavn, etternavn og klassekode).");
+      }
+      else
+      {
+          include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
 
-        $sqlSetning = "SELECT * FROM klasse WHERE klassekode='$klassekode';";
-        $sqlResultat = mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; hente data fra databasen");
-        $antallRader = mysqli_num_rows($sqlResultat); 
+          $sqlSetning = "SELECT * FROM student WHERE brukernavn='$brukernavn';";
+          $sqlResultat = mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; hente data fra databasen");
+          $antallRader = mysqli_num_rows($sqlResultat); 
 
-        if ($antallRader != 0)  /* klassen er registrert fra før */
-        {
-            print("Klassen er registrert fra f&oslash;r.");
-        }
-        else
-        {
-            $sqlSetning = "INSERT INTO klasse VALUES('$klassekode', '$klassenavn', '$studiumkode');";
-            mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; registrere data i databasen");
-              /* SQL-setning sendt til database-serveren */
+          if ($antallRader != 0)  /* studenten er registrert fra før */
+          {
+              print("Studenten er registrert fra f&oslash;r");
+          }
+          else
+          {
+              $sqlSetning = "INSERT INTO student VALUES('$brukernavn', '$fornavn', '$etternavn', '$klassekode');";
+              mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; registrere data i databasen");
 
-            print("F&oslash;lgende klasse er n&aring; registrert: $klassekode $klassenavn $studiumkode"); 
-        }
-    }
-}
+              print("F&oslash;lgende student er n&aring; registrert: $brukernavn $fornavn $etternavn ($klassekode)"); 
+          }
+      }
+  }
 ?> 
