f3369e29fa2d025473e1c2d269b73b52d29dc6b1
mathildesjo <mathildesjo@hotmail.com>
2025-10-26 22:20:59 +0100

    hhh

--- obligatoriskopg2/vis-alle-klasser.php
+++ obligatoriskopg2/vis-alle-klasser.php
@@ -1,27 +1,37 @@
 <?php  /* vis-alle-klasser */
 /*
-/*  Programmet skriver ut alle registrerte klasser
+   Programmet skriver ut alle registrerte klasser
 */
-include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+include("db-tilkobling.php");  /* kobler til databasen */
 
 $sqlSetning = "SELECT * FROM klasse;";
-$sqlResultat = mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; hente data fra databasen");
-/* SQL-setning sendt til database-serveren */
 
-$antallRader = mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
+$sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig &aring; hente data fra databasen");
+
+/* Beregn antall rader i resultatet */
+$antallRader = mysqli_num_rows($sqlResultat);
 
 print("<h3>Registrerte klasser</h3>");
-print("<table border=1>");  
-print("<tr><th align=left>Klassekode</th> <th align=left>Klassenavn</th> <th align=left>Studiumkode</th></tr>"); 
+print("<table border='1'>");
+print("<tr>
+        <th align='left'>Klassekode</th>
+        <th align='left'>Klassenavn</th>
+        <th align='left'>Studiumkode</th>
+      </tr>");
 
-while ($rad = mysqli_fetch_array($sqlResultat))  /* henter hver rad fra resultatet */
-{
+for ($r = 1; $r <= $antallRader; $r++) {
+    $rad = mysqli_fetch_array($sqlResultat);
     $klassekode = $rad["klassekode"];
     $klassenavn = $rad["klassenavn"];
     $studiumkode = $rad["studiumkode"];
 
-    print("<tr> <td>$klassekode</td> <td>$klassenavn</td> <td>$studiumkode</td> </tr>");
+    print("<tr>
+            <td>$klassekode</td>
+            <td>$klassenavn</td>
+            <td>$studiumkode</td>
+          </tr>");
 }
 
-print("</table>"); 
+print("</table>");
 ?>
