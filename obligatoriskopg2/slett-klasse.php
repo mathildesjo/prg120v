43c25c36b7fe392c93e400e0b1f24fcd0f214661
mathildesjo <mathildesjo@hotmail.com>
2025-10-26 22:24:19 +0100

    hhhh

--- obligatoriskopg2/slett-klasse.php
+++ obligatoriskopg2/slett-klasse.php
@@ -1,46 +1,38 @@
 <?php  /* slett-klasse */
 /*
-/*  Programmet lager et skjema for å velge en klasse som skal slettes  
-/*  Programmet sletter den valgte klassen
+   Programmet lager et skjema for å velge en klasse som skal slettes  
+   Programmet sletter den valgte klassen
 */
 ?> 
 
-<script src="funksjoner.js"> </script>
+<script src="funksjoner.js"></script>
 
 <h3>Slett klasse</h3>
 
 <form method="post" action="" id="slettKlasseSkjema" name="slettKlasseSkjema" onSubmit="return bekreft()">
-  Klassekode: <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
   <input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" /> 
 </form>
 
 <?php
-if (isset($_POST["slettKlasseKnapp"]))
-{	
+if (isset($_POST["slettKlasseKnapp"])) {	
     $klassekode = $_POST["klassekode"];
-	  
-    if (!$klassekode)
-    {
-        print("Klassekode m&aring; fylles ut");
-    }
-    else
-    {
-        include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+  
+    if (!$klassekode) {
+        print("Klassekode må fylles ut");
+    } else {
+        include("db-tilkobling.php");  /* kobler til database-serveren */
 
         $sqlSetning = "SELECT * FROM klasse WHERE klassekode='$klassekode';";
-        $sqlResultat = mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; hente data fra databasen");
+        $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig &aring; hente data fra databasen");
         $antallRader = mysqli_num_rows($sqlResultat); 
 
-        if ($antallRader == 0)  /* klassen finnes ikke */
-        {
+        if ($antallRader == 0) {
             print("Klassen finnes ikke");
-        }
-        else
-        {	  
+        } else {	  
             $sqlSetning = "DELETE FROM klasse WHERE klassekode='$klassekode';";
-            mysqli_query($db, $sqlSetning) or die("ikke mulig &aring; slette data i databasen");
-            /* SQL-setning sendt til database-serveren */
-		
+            mysqli_query($db, $sqlSetning) or die("Ikke mulig &aring; slette data i databasen");
+
             print("F&oslash;lgende klasse er n&aring; slettet: $klassekode <br />");
         }
     }
--- obligatoriskopg2/vis-klasse.php
+++ obligatoriskopg2/vis-klasse.php
@@ -1,47 +1,43 @@
 <?php  /* vis-klasse */
 /*
-/*  Programmet viser informasjon om én spesifikk klasse
+   Programmet lar brukeren skrive inn en klassekode
+   og viser informasjon om den valgte klassen.
 */
-?>
+?> 
 
 <h3>Vis klasse</h3>
 
-<form method="post" action="">
-  Klassekode: <input type="text" name="klassekode" required /> <br/>
-  <input type="submit" value="Vis klasse" name="visKlasseKnapp" />
+<form method="post" action="" id="visKlasseSkjema" name="visKlasseSkjema">
+  Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+  <input type="submit" value="Vis klasse" name="visKlasseKnapp" id="visKlasseKnapp" /> 
 </form>
 
 <?php
-if (isset($_POST["visKlasseKnapp"]))
-{
+if (isset($_POST["visKlasseKnapp"])) {
     $klassekode = $_POST["klassekode"];
 
-    if (!$klassekode)
-    {
-        print("Klassekode må fylles ut.");
-    }
-    else
-    {
-        include("db-tilkobling.php");  // kobling til databasen
+    if (!$klassekode) {
+        print("Klassekode må fylles ut");
+    } else {
+        include("db-tilkobling.php");  /* kobler til databasen */
 
         $sqlSetning = "SELECT * FROM klasse WHERE klassekode='$klassekode';";
-        $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig å hente data fra databasen");
+        $sqlResultat = mysqli_query($db, $sqlSetning) or die("Ikke mulig &aring; hente data fra databasen");
 
-        if (mysqli_num_rows($sqlResultat) == 0)
-        {
-            print("Klassen med kode '$klassekode' finnes ikke.");
-        }
-        else
-        {
+        $antallRader = mysqli_num_rows($sqlResultat);
+
+        if ($antallRader == 0) {
+            print("Klassen finnes ikke i databasen.");
+        } else {
             $rad = mysqli_fetch_array($sqlResultat);
             $klassenavn = $rad["klassenavn"];
             $studiumkode = $rad["studiumkode"];
 
-            print("<h4>Informasjon om klasse $klassekode</h4>");
-            print("<table border=1>");
-            print("<tr><th>Klassekode</th> <th>Klassenavn</th> <th>Studiumkode</th></tr>");
-            print("<tr><td>$klassekode</td> <td>$klassenavn</td> <td>$studiumkode</td></tr>");
-            print("</table>");
+            print("<h4>Informasjon om klassen:</h4>");
+            print("<table border='1'>
+                    <tr><th>Klassekode</th><th>Klassenavn</th><th>Studiumkode</th></tr>
+                    <tr><td>$klassekode</td><td>$klassenavn</td><td>$studiumkode</td></tr>
+                   </table>");
         }
     }
 }
